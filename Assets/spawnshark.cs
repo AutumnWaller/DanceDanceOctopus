96b2e7ea27b4c8ae8e63287fcd251152e2f80799
dean16jackson <dean49jackson@yahoo.co.uk>
2020-02-01 13:27:59 +0000

    dean

---
+++ Assets/spawnshark.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnshark : MonoBehaviour
+{
+    public GameObject shark;
+    public GameObject[] spawnpointsleft;
+    public GameObject[] spawnpointsright;
+    public int rand;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        rand = (Random.Range(0, 10));
+        Instantiate(shark, (spawnpointsleft[rand].transform.position), Quaternion.identity);
+    }
+}
