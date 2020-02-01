5f455ad1ced6c8d5a6748efb2a1d181d94a74a18
dean16jackson <dean49jackson@yahoo.co.uk>
2020-02-01 00:27:18 +0000

    dean boat

---
+++ Assets/Scripts/collisionboat.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionboat : MonoBehaviour
+{
+    public GameObject shark;
+    public GameObject decals;
+    public GameObject decals2;
+    int rand;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OncollisionEnter(Collision2D other)
+    {
+        rand = Random.Range(0, 2);
+        if (rand == 1)
+        {
+            decals.SetActive(true);
+        }
+        else
+        {
+            decals2.SetActive(true);
+        }
+    }
+}
