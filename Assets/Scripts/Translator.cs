ecac57ca3faee21d03a04bee68723bafa3bda74c
suma.amreiz <suma.amreiz@unity3d.com>
2020-02-01 17:01:11 +0000

    buoyancy done
    water physics for the game objects done

--- Assets/Scripts/Translator.cs
+++ Assets/Scripts/Translator.cs
@@ -7,9 +7,39 @@ public class Translator : MonoBehaviour
     [SerializeField]
     private float speed;
 
+    [SerializeField]
+    private float waterLevel = 4f;
+    
+    [SerializeField]
+    private float floatHeight = 2f;
+
+    [SerializeField]
+    private float bounceDamp = 0.05f;
+
+    [SerializeField]
+    private Vector3 BuoyancyCenterOffset;
+
+    [SerializeField]
+    private float forceFactor;
+
+    private Vector3 actionPoint;
+
+    private Vector3 upLift;
+
     // Update is called once per frame
-    void Update()
+
+    Rigidbody rigidbody;
+
+    private void Update()
     {
+        actionPoint = transform.position + transform.TransformDirection(BuoyancyCenterOffset);
+        forceFactor = 1f - ((actionPoint.y - waterLevel) / floatHeight);
         transform.Translate(Vector3.back * speed);
+
+        if(forceFactor > 0f)
+        {
+            upLift = -Physics.gravity * (forceFactor - rigidbody.velocity.y * bounceDamp);
+            rigidbody.AddForceAtPosition(upLift, actionPoint);
+        }
     }
 }
