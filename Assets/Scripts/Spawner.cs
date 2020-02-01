8f2b1143a4a8d50983ccc17385d7f659d3ae4adc
suma.amreiz <suma.amreiz@unity3d.com>
2020-02-01 16:04:39 +0000

    Suma's spawner code
    Made a spawner, will work on bobbing effect now.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,18 +1,57 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // list of objects
+    List<GameObject> listOfObj = new List<GameObject>();
+
+    // Instance of pools
+    PoolManager poolManager;
+
+    // Will set each generated object to the initial starting postion of the Spawner object
+    Vector3 currentPostion;
+
+    // This will be matched up with Time.time
+    private float nextSpawnTime;
+
+    // The wood to be spawned
+    [SerializeField]
+    private GameObject currentObj;
+
+    // How long you wanna delay each object being spawned
+    [SerializeField]
+    private float spawnDelay = 10f;
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // pretty self explanatory
+        if(ShouldSpawn())
+        {
+            Spawn();
+        }
+    }
+
+    private void Awake()
+    {
+        currentPostion = transform.position;
+        // Make a pool of how many objects you want
+        PoolManager.CreatePool(currentObj, 30, ref listOfObj);
+    }
+
+    private bool ShouldSpawn()
+    {
+        return Time.time >= nextSpawnTime;
+    }
+
+    private void Spawn()
+    {
+        nextSpawnTime = Time.time + spawnDelay;
+        GameObject setWoodPostion = PoolManager.GetObjectFromPool(listOfObj);
+        setWoodPostion.transform.position = currentPostion;
+        setWoodPostion.SetActive(true);
     }
 }
