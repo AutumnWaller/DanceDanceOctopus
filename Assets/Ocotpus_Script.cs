5be6ca1e2877b52b4f9ba11afd156838f2f53aec
JacobSimpson-unity <53442088+JacobSimpson-unity@users.noreply.github.com>
2020-02-01 11:54:07 +0000

    Jacob - Octopus start
    Set up a visualizer and set up octopus ready to be animated

---
+++ Assets/Ocotpus_Script.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+//We're all going to die, what does it matter
+public class Ocotpus_Script : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] limbsGO;
+    Animator[] limbs;
+    [SerializeField]
+    KeyCode Tentacle_01, Tentacle_02,Tentacle_03,Tentacle_04,Tentacle_05,Tentacle_06;
+    [SerializeField]
+    bool[] canGrab;
+
+
+    void Start()
+    {
+        for (int i = 0; i < limbsGO.Length; i++)
+        {
+            limbs[i] = limbsGO[i].GetComponent<Animator>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(Tentacle_01))
+        {
+            if(canGrab[0]==true)
+            {
+                limbs[0].SetTrigger("Grab");
+            }
+        }
+
+    }
+}
